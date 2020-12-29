f96e7dee744cfe6b176b5c53398293ea78e40f57
DarinkaTownsend <66121094+DarinkaTownsend@users.noreply.github.com>
2020-12-29 17:17:23 -0500

    Interfaz final

--- Grupo_7/src/Interfaz/Main.java
+++ Grupo_7/src/Interfaz/Main.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author darin
  */
-public class JavaFXApplication7 extends Application {
+public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
